c7ceb015d08afa3803e2831d6b01e0779de28a4e
Ikenna N Eke <ieke@mail.sfsu.edu>
2021-02-24 15:16:06 -0800

    Fixed

--- Assets/Resources/Scripts/SceneSwitch.cs
+++ Assets/Resources/Scripts/SceneSwitch.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     public void SwitchToScene1(){
-        SceneManager.LoadScene("Scene 1");
+        SceneManager.LoadScene("MergeScene");
     }
 
     public void switchToScene2(){
